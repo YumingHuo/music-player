1f5ccb02711ba58a5183e2fa3284a847391737c3
YumingHuo <hym811@outlook.com>
2023-03-22 18:47:00 +0000

    Initial setup complete

---
+++ player/src/views/Root.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+
+const msg = ref('首页')
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <div class="box">
+    <ul>
+      <li>12123</li>
+      <li>12123</li>
+      <li>12123</li>
+      <li>12123</li>
+    </ul>
+    <p>niahoahao</p>
+  </div>
+
+</template>
+
+<style scoped lang="scss">
+
+$orangeColor:#f60;
+$greyColor:#f3f3f3; 
+  .box {
+    color: $orangeColor;
+    ul li {
+      background: $greyColor;
+    }
+    p {
+      color: $orangeColor;
+    }
+  }
+</style>
--- player/src/views/music/index.vue
+++ player/src/views/music/index.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 
+
+const msg = ref('音乐')
 </script>
 
 <template>
-  <div class="content">
-    <router-view />
-  </div>
+  <h1>{{ msg }}</h1>
+
 </template>
 
 <style scoped>
